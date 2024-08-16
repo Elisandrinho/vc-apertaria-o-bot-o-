4864e6712d51ea0247e0b6259bea0445f7049ba8
Elisandrinho <106559520+Elisandrinho@users.noreply.github.com>
2024-08-16 11:47:18 -0300

    a

---
+++ script.js
@@ -0,0 +1,50 @@
+const caixaPrincipal = document.querySelector('caixa-principal');
+const caixaPerguntas = document.querySelector('caixa-pergumtas');
+const caixaAlternativas = document.querySelector('caixa-alternativas');
+const caixaResultado = document.querySelector('caixa-resultado');
+const textoResultado = document.querySelector('texto-principal');
+
+const pergumtas = [ 
+    { 
+        enunciado: "no âmbito social, você prefere:",
+        alternativas: [ 
+            { 
+                texto: "alternativa 1",
+                afirmação: "afirmação", 
+            },
+            { 
+                texto: "alternativa 2",
+                afirmação: "afirmação", 
+            }, 
+        ] 
+    },
+    { 
+        enunciado: "no âmbito ambieltal, você prefere:",
+        alternativas: [ 
+            { 
+                texto: "alternativa 1",
+                afirmação: "afirmação", 
+            },
+            { 
+                texto: "alternativa 2",
+                afirmação: "afirmação", 
+            }, 
+        ] 
+    },
+    { 
+        enunciado: "no âmbito tecnologico, você prefere:",
+        alternativas: [ 
+            { 
+                texto: "alternativa 1",
+                afirmação: "afirmação", 
+            },
+            { 
+                texto: "alternativa 2",
+                afirmação: "afirmação", 
+            }, 
+        ] 
+    },
+]
+
+let atual = 0;
+let pergtunaAtual;
